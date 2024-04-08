2461445a9c344aa40dc75de4a62a93234f6330fd
Sorin Giurgica <H3XM@it7.eurofins.com>
2024-04-08 09:25:37 +0200

    Initial commit

---
+++ AngularAuthAPI/AngularAuthAPI/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using AngularAuthAPI.Context;
+using AngularAuthAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularAuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(AppDbContext authContext) : ControllerBase
+    {
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            var user = await authContext.Users.FirstOrDefaultAsync(x => x.Username == userObj.Username && x.Password == userObj.Password);
+
+            if (user == null)
+                return NotFound(new { Message = "User not found!" });
+
+            return Ok(new { Message = "Login Success" });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            await authContext.Users.AddAsync(userObj);
+            await authContext.SaveChangesAsync();
+            return Ok(new { Message = "User registered!" });
+        }
+    }
+}
---
+++ AngularAuthAPI/AngularAuthAPI/Program.cs
@@ -0,0 +1,42 @@
+using AngularAuthAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("MyPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+    });
+});
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer"));
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("MyPolicy");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
